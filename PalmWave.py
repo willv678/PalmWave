d676fd787dc4c759690b6b9f51826d3baf20c47d
Will <willv678@gmail.com>
2023-06-16 16:50:49 -0400

    right click support

--- PalmWave.py
+++ PalmWave.py
@@ -9,6 +9,9 @@ def perform_click():
     # Super mega nerd emoji geeksforgeeks told me to try the euclidien formula so i did
     pyautogui.click()
 
+def perform_right__click():
+    pyautogui.rightClick()
+
 def move_mouse(x, y):
     # Move the mouse based on the x and y coordinates
     screen_width, screen_height = pyautogui.size()
@@ -28,13 +31,13 @@ def main():
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
     #I'm not sure about this frame counter thing, cuz it should reduce latency but also it would mean the mouse is a lot less accurate as it pings less often
-    frame_counter = 0
+    #frame_counter = 0
     while True:
         ret, frame = cap.read()
-        frame_counter += 1
+        #frame_counter += 1
 
-        if frame_counter % 2 != 0:
-            continue
+        #if frame_counter % 2 != 0:
+            #continue
 
         # Flip the frame horizontally
         frame = cv2.flip(frame, 1)
@@ -50,18 +53,25 @@ def main():
                 # Get the landmarks for each finger
                 thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
                 index_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                middle_tip = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
 
                 # Get the pixel coordinates of the finger tips
                 thumb_x, thumb_y = int(thumb_tip.x * frame.shape[1]), int(thumb_tip.y * frame.shape[0])
                 index_x, index_y = int(index_tip.x * frame.shape[1]), int(index_tip.y * frame.shape[0])
+                middle_x, middle_y = int(middle_tip.x * frame.shape[1]), int(middle_tip.y * frame.shape[0])
 
                 # Calculate the distance between the index and thumb coordinates
-                distance = ((index_x - thumb_x) ** 2 + (index_y - thumb_y) ** 2) ** 0.5
+                left_click_distance = ((index_x - thumb_x) ** 2 + (index_y - thumb_y) ** 2) ** 0.5
+                right_click_distance = ((middle_x - thumb_x) ** 2 + (middle_y - thumb_y) ** 2) ** 0.5
 
                 # Print a message when the index and thumb are close
-                if distance < CLICK_DISTANCE_THRESHOLD:
+                if left_click_distance < CLICK_DISTANCE_THRESHOLD:
                     print("Click detected!")
                     perform_click()
+                
+                if right_click_distance < CLICK_DISTANCE_THRESHOLD:
+                    print("Right click detected!")
+                    perform_right__click()
 
                 # Move the mouse based on the index finger movement
                 move_mouse(index_tip.x, index_tip.y)
@@ -69,9 +79,11 @@ def main():
                 # Draw circles at the finger tip positions
                 cv2.circle(frame, (thumb_x, thumb_y), 5, (0, 255, 0), -1)
                 cv2.circle(frame, (index_x, index_y), 5, (0, 255, 0), -1)
+                cv2.circle(frame, (middle_x, middle_y), 5, (100, 200, 0), -1)
+                
 
         # Display the frame with overlays
-        cv2.imshow("Hand Tracking", frame)
+        cv2.imshow("PalmWave Camera View", frame)
 
         # Exit the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
